bc7947ba6efc83577b42b435b524d5a185538360
sama.mahmoud <sama.mahmoud@cloud.statcan.ca>
2023-03-03 13:55:29 +0000

    updated documentation

--- examples/standard.tf
+++
@@ -1,2 +0,0 @@
-// MSSQL Terraform: https://github.com/canada-ca-terraform-modules/terraform-azurerm-mssql/blob/master/examples/standard.
-// See https://learn.microsoft.com/en-us/azure/azure-sql/database/service-tiers-dtu?view=azuresql#elastic-pool-limits for more
